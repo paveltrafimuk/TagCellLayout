740730c021d8acba30c0336991d59045c6ac4860
Pavel Trafimuk <paveltrafimuk@icloud.com>
2019-12-31 08:12:32 +0300

    updated SPM

--- Package.swift
+++ Package.swift
@@ -1,10 +1,8 @@
 // swift-tools-version:5.1
 import PackageDescription
 
-let package = Package(name: "TagCellLayout",
-                      platforms: [.iOS(.v9.1)],
-products: [.library(name: "TagCellLayout",
-                    targets: ["TagCellLayout"])],
-targets: [.target(name: "TagCellLayout",
-                  path: "TagCellLayout/TagCellLayout")],
-swiftLanguageVersions: [.v5])
+let package = Package(
+    name: "TagCellLayout",
+    dependencies: [
+    ]
+)
