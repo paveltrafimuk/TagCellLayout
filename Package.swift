14a5e84c548f3068286e97918cfb82f649d4eafe
Pavel Trafimuk <paveltrafimuk@icloud.com>
2019-12-31 08:33:41 +0300

    fixed SPM#4

--- Package.swift
+++ Package.swift
@@ -3,19 +3,16 @@ import PackageDescription
 
 let package = Package(
     name: "TagCellLayout",
-    dependencies: [
-    ],
     products: [
-           .library(
-               name: "TagCellLayout",
-               targets: ["TagCellLayout"])
-       ],
+        .library(
+            name: "TagCellLayout",
+            targets: ["TagCellLayout"])
+    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "TagCellLayout",
-            dependencies: [])
+            name: "TagCellLayout")
     ]
     
 )
